da15ef29130de347cead3d3f95cc40d3759b2227
Alpha Diallo <aob.diallo@gmail.com>
2018-10-01 11:18:48 +0200

    - run for loop sequentially

--- concurrency/001.go
+++ concurrency/001.go
@@ -1,33 +1,58 @@
-package concurrency
+package main
 
 import "fmt"
 
 type Person struct {
-	Age int
-	Name string
+	Age      int
+	Name     string
 	Nickname string
 }
 
 type Persons []Person
 
-var developers = []Person{
-	{
-		Age:20,
-		Name: "alpha",
-		Nickname: "Ghost",
-	},
-}
-
-func (p *Person) ToString() string  {
-
+func (p Person) ToString() string {
+	return fmt.Sprintf("Name=%s Nickname=%s, Age=%d", p.Name, p.Nickname, p.Age)
 }
 
 func (p *Person) Talk() {
 	fmt.Printf("Hello my name is %s, i'm %d years old and my nickname is %s booya\n", p.Name, p.Age, p.Nickname)
 }
 
-func SayHello(persons Persons)  {
-	for i := 0; i < len(persons); i++ {
+var peoples = Persons{
+	{
+		Age:      18,
+		Name:     "Alpha",
+		Nickname: "OumarPoulo",
+	},
+	{
+		Age:      14,
+		Name:     "Bambewel",
+		Nickname: "Majesty",
+	},
+	{
+		Age:      15,
+		Name:     "Aissatou",
+		Nickname: "Flash",
+	},
+	{
+		Age:      10,
+		Name:     "Idrissa",
+		Nickname: "Paikoun",
+	},
+}
 
+func main() {
+	// peoples will talk sequentially
+	for key, people := range peoples {
+		fmt.Println("current person == key:", key, " people=", people)
+		people.Talk()
 	}
+	// current person == key: 0  people= {18 Alpha OumarPoulo}
+	// Hello my name is Alpha, i'm 18 years old and my nickname is OumarPoulo booya
+	// current person == key: 1  people= {14 Bambewel Majesty}
+	// Hello my name is Bambewel, i'm 14 years old and my nickname is Majesty booya
+	// current person == key: 2  people= {15 Aissatou Flash}
+	// Hello my name is Aissatou, i'm 15 years old and my nickname is Flash booya
+	// current person == key: 3  people= {10 Idrissa Paikoun}
+	// Hello my name is Idrissa, i'm 10 years old and my nickname is Paikoun booya
 }
